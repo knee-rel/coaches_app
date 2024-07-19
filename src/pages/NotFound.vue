fc70bd9f74db22c18cb2b6d0674526cc856a1ebd
Nirel Ibarra <62606372+knee-rel@users.noreply.github.com>
2024-07-19 22:18:20 +0800

    first commit

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Page Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
